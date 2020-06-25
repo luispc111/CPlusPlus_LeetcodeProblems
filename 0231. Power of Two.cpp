bfda6f241079ba0893b0c0a7da341299e68b3fde
Luis Alberto Pérez Chapa <31547300+luispc111@users.noreply.github.com>
2020-06-24 20:36:01 -0500

    Add files via upload

---
+++ 0231. Power of Two.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        while(n%2 == 0 && n > 0){
+            n = n/2;
+        }
+        
+        if(n == 1){
+            return true;
+        }
+        
+        return false;
+    }
+};
